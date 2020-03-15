f234c8b3212506733a10b447e0fe6fc3e0b7077c
David Liu <chihweiliu1993@gmail.com>
2020-03-15 00:08:51 +0000

    tweak card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,17 +3,42 @@ import Proptypes from 'prop-types'
 import styled from 'styled-components'
 import InformationRow from './InfromationRow'
 
+const Wrapper = styled.section``
 const ProjectDetails = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  grid-auto-rows: minmax(0px, 1fr);
-  grid-gap: 25px;
+  display: flex;
+  min-width: 200px;
+  justify-content: space-between;
+  width: 100%;
+  @media (max-width: 600px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`
+
+const Card = styled.div`
+  box-shadow: 2px 4px 25px rgba(0, 0, 0, 0.1);
+  padding: 30px;
+  border-radius: 10px;
 `
 
 const DetailBorder = styled.div`
-  border-radius: 10px;
-  border: 1px solid lightgray;
-  padding: 25px;
+  width: 50%;
+  @media (max-width: 600px) {
+    width: 100%;
+  }
+  padding: 30px;
+`
+
+const Spacer = styled.div`
+  width: 1px;
+  height: 90px;
+  background-color: lightgrey;
+  align-self: center;
+  @media (max-width: 600px) {
+    width: 90px;
+    height: 1px;
+    justify-content: center;
+  }
 `
 
 const Header = styled.div`
@@ -28,7 +53,6 @@ const StyledImage = styled.img`
   height: 100%;
   width: 100%;
   object-fit: cover;
-  box-shadow: 9px 10px 59px -19px rgba(0, 0, 0, 0.41);
 `
 
 const P = styled.p`
@@ -51,48 +75,52 @@ const ProjectCard = ({ project, toggleLightbox }) => {
   const mainImage = project.images[0]
 
   return (
-    <article>
+    <Wrapper>
       <h3>{project.title}</h3>
-      <ImageLinkWrapper
-        className="image fit thumb"
-        href={mainImage.source}
-        onClick={e => {
-          e.preventDefault()
-          toggleLightbox(project)
-        }}
-      >
-        <StyledImage src={mainImage.source} alt={mainImage.description} />
-      </ImageLinkWrapper>
+      <Card>
+        <ImageLinkWrapper
+          className="image fit thumb"
+          href={mainImage.source}
+          onClick={e => {
+            e.preventDefault()
+            toggleLightbox(project)
+          }}
+        >
+          <StyledImage src={mainImage.source} alt={mainImage.description} />
+        </ImageLinkWrapper>
 
-      <ProjectDetails>
-        <DetailBorder>
-          <Header>About</Header>
-          <P>{project.description}</P>
-        </DetailBorder>
+        <ProjectDetails>
+          <DetailBorder>
+            <Header>About</Header>
+            <P>{project.description}</P>
+          </DetailBorder>
 
-        <DetailBorder>
-          <Header>Tech Stack</Header>
-          <InformationRow
-            title="Front End"
-            value={project.techStack.frontEnd}
-          />
-          {project.techStack.backEnd && (
+          <Spacer />
+
+          <DetailBorder>
+            <Header>Tech Stack</Header>
             <InformationRow
-              title="Back End"
-              value={project.techStack.backEnd}
+              title="Front End"
+              value={project.techStack.frontEnd}
             />
-          )}
-          <LinksWrapper>
-            {project.githubLinks.map(link => (
-              <Link href={link.url}>{link.label}</Link>
-            ))}
-            {project.liveSiteLink && (
-              <Link href={project.liveSiteLink}>Live Site</Link>
+            {project.techStack.backEnd && (
+              <InformationRow
+                title="Back End"
+                value={project.techStack.backEnd}
+              />
             )}
-          </LinksWrapper>
-        </DetailBorder>
-      </ProjectDetails>
-    </article>
+            <LinksWrapper>
+              {project.githubLinks.map(link => (
+                <Link href={link.url}>{link.label}</Link>
+              ))}
+              {project.liveSiteLink && (
+                <Link href={project.liveSiteLink}>Live Site</Link>
+              )}
+            </LinksWrapper>
+          </DetailBorder>
+        </ProjectDetails>
+      </Card>
+    </Wrapper>
   )
 }
 
