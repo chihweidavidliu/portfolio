47bb78355fb39005b0d5961de0975d33f5ab466b
David Liu <chihweiliu1993@gmail.com>
2020-03-15 10:29:27 +0000

    cleaned up styles

--- src/components/HomePageSections/ContactForm.js
+++ src/components/HomePageSections/ContactForm.js
@@ -8,75 +8,43 @@ const ContactFormWrapper = styled.section`
 `
 
 const InnerWrapper = styled.div`
+  width: 100%;
   max-width: 800px;
 `
+
+const StyledForm = styled.form`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-self: center;
+`
+
+const TopRow = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+  margin-bottom: 20px;
+`
+
 const ContactForm = () => (
   <ContactFormWrapper id="three">
     <InnerWrapper>
-      <h2>Get In Touch</h2>
-      <p>
-        Accumsan pellentesque commodo blandit enim arcu non at amet id arcu
-        magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem
-        vulputate lorem neque lorem ipsum dolor.
-      </p>
-      <div className="row">
-        <div className="8u 12u$(small)">
-          <form method="post" action="#">
-            <div className="row uniform 50%">
-              <div className="6u 12u$(xsmall)">
-                <input type="text" name="name" id="name" placeholder="Name" />
-              </div>
-              <div className="6u 12u$(xsmall)">
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Email"
-                />
-              </div>
-              <div className="12u">
-                <textarea
-                  name="message"
-                  id="message"
-                  placeholder="Message"
-                  rows="4"
-                ></textarea>
-              </div>
-            </div>
-          </form>
-          <ul className="actions">
-            <li>
-              <input type="submit" value="Send Message" />
-            </li>
-          </ul>
-        </div>
-        <div className="4u 12u$(small)">
-          <ul className="labeled-icons">
-            <li>
-              <h3 className="icon fa-home">
-                <span className="label">Address</span>
-              </h3>
-              1234 Somewhere Rd.
-              <br />
-              Nashville, TN 00000
-              <br />
-              United States
-            </li>
-            <li>
-              <h3 className="icon fa-mobile">
-                <span className="label">Phone</span>
-              </h3>
-              000-000-0000
-            </li>
-            <li>
-              <h3 className="icon fa-envelope-o">
-                <span className="label">Email</span>
-              </h3>
-              <a href="#">hello@untitled.tld</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <h2>Contact</h2>
+      <p>Interested in my work? Get in touch here!</p>
+
+      <StyledForm method="post" action="#">
+        <TopRow>
+          <input type="text" name="name" id="name" placeholder="Name" />
+          <input type="email" name="email" id="email" placeholder="Email" />
+        </TopRow>
+
+        <textarea name="message" id="message" placeholder="Message" rows="4" />
+      </StyledForm>
+      <ul className="actions">
+        <li>
+          <input type="submit" value="Send Message" />
+        </li>
+      </ul>
     </InnerWrapper>
   </ContactFormWrapper>
 )
