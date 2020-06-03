b7b313c4f7f285b319cf2b0797aa1df4c06c7f81
David Liu <chihweiliu1993@gmail.com>
2020-06-03 18:09:03 +0100

    update meteors

--- src/projects/meteors.js
+++ src/projects/meteors.js
@@ -4,7 +4,7 @@ export const meteorsProject = {
   id: uuidv4(),
   title: 'Meteors',
   description:
-    '(WIP) A game to help make learning more fun. Users input flash cards to be used as part of the game.',
+    'A game to help make learning more fun. Users input flash cards to be used as part of the game. Includes local storage saved lists and persisted learning stats',
   techStack: {
     frontEnd: 'React, Typescript, Styled Components',
   },
@@ -18,7 +18,7 @@ export const meteorsProject = {
   images: [
     {
       source:
-        'https://res.cloudinary.com/dhccfu1un/image/upload/v1591012741/Meteors/Screenshot_2020-06-01_at_12.40.00_fybpbj.png',
+        'https://res.cloudinary.com/dhccfu1un/image/upload/v1591204018/Meteors/Screenshot_2020-06-03_at_18.06.30_n5qgz4.png',
       caption: 'Home',
     },
     {
