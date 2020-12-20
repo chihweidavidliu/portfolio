3e4252ec50fbe929cfeb339384bbca74cd47384f
David Liu <chihweiliu1993@gmail.com>
2020-12-20 10:10:26 +0000

    initial

---
+++ src/components/HomePageSections/Timeline.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import SectionHeader from '../typography/SectionHeader'
+import Section from '../Section'
+
+const Timeline = () => (
+  <Section>
+    <SectionHeader>Timeline</SectionHeader>
+  </Section>
+)
+
+export default Timeline
